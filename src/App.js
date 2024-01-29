f7f45e91fb52a29c8580f33578fde0e1cefd1996
Dallin <bowiedallin@gmail.com>
2024-01-29 14:28:00 -0700

    functional tic tac toe game

--- src/App.js
+++ src/App.js
@@ -14,8 +14,10 @@ export default function Board() {
 	const [squares, setSquares] = useState(Array(9).fill(null)); //initialize an array of length 9. each value is null.
 
 	function handleClick(index) {
-		if (squares[index]) {//check if the value of the square is not null
-			return; //if the square has already been chosen, don't let the user pick it!
+		//check if the value of the square is not null
+		//check if a player has already won the game
+		if (squares[index] || calculateWinner(squares)) {
+			return; //if the square has already been chosen or the game is over, don't let the user pick it!
 		}
 
 		const nextSquares = squares.slice(); //make a copy of the squares array
@@ -30,8 +32,18 @@ export default function Board() {
 		setXIsNext(!xIsNext);
 	}
 
+	//add text that displays game status. Who goes next or who won.
+	const winner = calculateWinner(squares);
+	let status;
+	if (winner) {
+		status = "Winner: " + winner;
+	} else {
+		status = "Next player: " + (xIsNext ? "X" : "O");
+	}
+
 	return (
 		<>
+		<div className='status'>{status}</div>
 		  <div className="board-row">
 			<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
 			<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -49,4 +61,26 @@ export default function Board() {
 		  </div>
 		</>
 	  );
+
+
+	  function calculateWinner(squares) {
+		//take in the squares array and check if there is a winner (x, o, null)
+		const lines = [
+		  [0, 1, 2],
+		  [3, 4, 5],
+		  [6, 7, 8],
+		  [0, 3, 6],
+		  [1, 4, 7],
+		  [2, 5, 8],
+		  [0, 4, 8],
+		  [2, 4, 6]
+		];
+		for (let i = 0; i < lines.length; i++) {
+		  const [a, b, c] = lines[i];
+		  if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		  }
+		}
+		return null;
+	  }
   }
